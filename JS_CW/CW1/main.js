9421f1faf10b51e4a223140b2ca127711966a440
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-03-07 22:35:24 +0500

    CW2

---
+++ JS_CW/CW1/main.js
@@ -0,0 +1,47 @@
+// // let var const
+// let a = 14;
+
+// let num1 = 10;
+// let num2 = 3;
+// let sum = num1 + num2;
+// console.log('sadadasdasd sda ravna' + sum)
+// console.log(num1+num2, num1 - num2)
+
+// let str1 = '14';
+// let num = 10;
+// console.log(str1 + num, str1 - num)
+
+// const res = 2 
+// let surge =  +prompt("S kakogo goda brat?: ")
+// let resres = surge ** res
+// console.log(resres)
+
+// const res = 2 
+// let surge1 =  +prompt("S kakogo goda brat?: ");
+// let surge2 =  +prompt("S kakogo goda brat2?: ");
+// let resres = (surge1+surge2)/2;
+// console.log(resres);
+
+// const m = 621371;
+// let surge1 =  +prompt("S kakogo goda brat?: ");
+// let resres = surge1 * 2;
+// console.log(resres);
+
+// let $userNumber1 = +prompt("Enter number: " );
+// let $userNumber2 = +prompt("Enter number: " );
+// let mult = $userNumber1*$userNumber2;
+// let add = $userNumber1+$userNumber2;
+// let subtract = $userNumber1-$userNumber2;
+// let division = $userNumber1/$userNumber2;
+// alert("addition result: " + add + "\nsubraction result: " + subtract+ "\ndivision result: " + division+ "\nmultiplication result: " + mult);
+
+// let a = +prompt("Enter number: " );
+// let b = +prompt("Enter number: " );
+// let x = -b/a;
+// alert("The x = " + x);
+
+let a = +prompt("Enter number: " );
+let b = +prompt("Enter number: " );
+let at = 23-a;
+let ab = 60-b;
+alert(at + ':' + ab);
---
+++ JS_CW/CW2/main.js
@@ -0,0 +1,92 @@
+// let t = +prompt('time')
+// if(t < 0){
+//     console.log('положительное')
+// }
+// else if(t === 0){
+//     console.log('нейтралка')
+// }
+// else{
+//     console.log('отрицалово')
+// }
+
+// let t = +prompt('сколько годиков?')
+// if(t >= 0 && t <= 120){
+//     console.log('Нормалин')
+// }
+// else{
+//     console.log('ЛОХ')
+// }
+
+// let t = +prompt('где сыыыыыр?')
+// let ttt = Math.abs(t)
+// alert(`ЫЫЫЫЫЫ: ${ttt}`)
+
+// let t1 = +prompt('час?')
+// let t2 = +prompt('минута?')
+// let t3 = +prompt('сэкунда?')
+
+// if(t1 >= 00 && t1 <= 23){
+//     console.log(t1)
+// }
+// else{
+//     console.log("хня, исправляй" + t1)
+// }
+
+// if(t2 >= 00 && t2 <= 59){
+//     console.log(t2)
+// }
+// else{
+//     console.log("хня, исправляй" + t2)
+// }
+
+// if(t3 >= 00 && t3 <= 59){
+//     console.log(t1)
+// }
+// else{
+//     console.log("хня, исправляй" + t1)
+// }
+
+// alert(`Ваше время ${t1}:${t2}:${t3}`)
+
+// var nomerMesyaza = prompt("vvedi nomer mesyaza: ")
+
+// switch(nomerMesyaza){
+//     case '1':
+//         console.log('Yanvar')
+//     break;
+//     case '2':
+//         console.log('Fevral')
+//     break;
+//     case '3':
+//         console.log('Mart')
+//     break;
+//     case '4':
+//         console.log('Aprel')
+//     break;
+//     case '5':
+//         console.log('May')
+//     break;
+//     case '6':
+//         console.log('iun')
+//     break;
+//     case '7':
+//         console.log('Iyul')
+//     break;
+//     case '8':
+//         console.log('Avgust')
+//     break;
+//     case '9':
+//         console.log('Senryabr')
+//     break;
+//     case '10':
+//         console.log('Oktabr')
+//     break;
+//     case '11':
+//         console.log('Noyabr')
+//     break;
+//     case '12':
+//         console.log('Dekabr')
+//     break;
+// }
+
+

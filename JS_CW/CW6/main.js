9c540100e8ac1cb87ac476ac16e8b877a4a447b7
T1M0W4 <150163671+T1M0W4@users.noreply.github.com>
2024-03-29 21:45:54 +0500

    CW6

---
+++ JS_CW/CW6/main.js
@@ -0,0 +1,74 @@
+// let array = [1, 'hello', function(){alert('hello')}, true, {model:'Toyota'}]
+// console.log(array.length)
+// console.log(array[1])
+// console.log(array[2]())
+// console.log(array[4].model)
+
+//------------
+
+//pop push unshift shift slice(start, end)
+// let array2 = [1,2,3,4]
+// array2.pop()
+// console.log(array2)
+// array2.push(5, 'Hello')
+// console.log(array2)
+// array2.unshift(0, 2, 4)
+// console.log(array2)
+// array2.shift()
+// console.log(array2)
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let array = arr.slice(2, 4)
+// console.log(array)
+
+//------------
+
+// let arr = [1, 'База', true, function(){alert('Салам!')}]
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof(arr[i]) === function()) {
+//     console.log(arr[i])
+// }
+// }
+// console.log()
+
+//------------
+
+// let random = Math.trunc(Math.random() * 10) + 1
+// console.log(random)
+
+//------------
+
+//1
+// let array = []
+// console.log(array);
+
+// for (let i = 0; i<10;i++){
+//     array.push(Math.trunc(Math.random() * 10))
+//       }
+// console.log(array);
+
+
+// let minIndex = 0;
+// let maxIndex = 0;
+// for (let i = 1; i < array.length; i++) {
+//     if (array[i] < array[minIndex]) {
+//         minIndex = i;
+//     }
+//     if (array[i] > array[maxIndex]) {
+//         maxIndex = i;
+//     }
+// }
+
+// // Меняем местами минимальный и максимальный элементы
+// let temp = array[minIndex];
+// array[minIndex] = array[maxIndex];
+// array[maxIndex] = temp;
+
+// // Выводим измененный массив
+// console.log("Массив после замены минимального и максимального элементов:");
+// console.log(array);
+
+//-------------
+
